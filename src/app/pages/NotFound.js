de383f60496e82c250854118b5d4db1d9f94e1bc
Ignacio Montero <ig_montero@hotmail.com>
2020-07-09 11:58:53 -0300

    Not found page translations

--- src/app/pages/NotFound.js
+++ src/app/pages/NotFound.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Routes from '../constants/Routes';
+import translationKeys from '../../core/localization/translations/translationKeys.json';
+import { withTranslation } from 'react-i18next';
 
-const NotFound = () => {
+const NotFound = (props) => {
+    const { t } = props;
+    
     return (<div style={{ height: '100vh' }} className="d-flex flex-column w-100 align-items-center justify-content-center text-center">
-        <h1>Page not found.</h1> 
+        <h1>{t(translationKeys.pageNotFound)}</h1>
         <br />
-        <Link to={Routes.home.url}>Back to homepage.</Link>
+        <Link to={Routes.home.url}>{t(translationKeys.backToHomepage)}</Link>
     </div>)
 }
 
-export default NotFound;
+export default withTranslation()(NotFound);
